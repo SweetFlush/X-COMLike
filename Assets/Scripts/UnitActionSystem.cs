55d6cf93b22d357add922ea867b58c0ca69570ca
SweetFlush <kun1998@naver.com>
2022-10-19 00:28:06 +0900

    Unit Select 2

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -1,11 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
+    public static UnitActionSystem Instance { get; private set;}
+
+    public event EventHandler OnSelectedUnitChanged;
+
+
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayerMask;
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+    }
 
     private void Update()
     {
@@ -20,6 +35,7 @@ public class UnitActionSystem : MonoBehaviour
         }
     }
 
+    //마우스 클릭으로 선택된 유닛을 바꿈
     private bool TryHandleUnitSelection()
     {
         Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -27,11 +43,27 @@ public class UnitActionSystem : MonoBehaviour
         {
             if(raycastHit.transform.TryGetComponent(out Unit unit))
             {
-                selectedUnit = unit;
+                SetSelectedUnit(unit);
                 return true;
             }
         }
 
         return false;
+    }
+
+    //선택된 유닛을 바꾸고 이벤트를 fire 하는 함수
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+        //if (OnSelectedUnitChanged != null)
+        //{
+        //    OnSelectedUnitChanged(this, EventArgs.Empty);
+        //}
+    }
+
+    public Unit GetSelectedUnit() {
+        return selectedUnit;
     }
 }
